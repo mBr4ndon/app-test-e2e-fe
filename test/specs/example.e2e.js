3973dd4644bd567598aa4bfdf5eb92e1fddc8827
Marcio Brandao (CTW) <Marcio.Brandao@ctw.bmwgroup.com>
2022-03-10 18:10:58 +0000

    WebdriverIO basic test

---
+++ test/specs/example.e2e.js
@@ -0,0 +1,46 @@
+const axios = require('axios');
+const { Client } = require('pg');
+
+describe('My Login application', () => {
+
+    beforeEach(async () => {
+        await axios.delete('http://localhost:3333/posts', {
+            title: 'Title test',
+            description: 'Description test'
+        });
+    });
+
+    it('should visit a page', async () => {
+        // visit page
+        await browser.url('/');
+
+        // find elements
+        const titleInput = await $("input[name=title]");
+        const descriptionTitle = await $("input[name=description]");
+        const button = await $("button");
+
+        // fill form and submit
+        await titleInput.setValue("Title test");
+        await descriptionTitle.setValue("Description test");
+        await button.click();
+
+        // check if h2 appeared
+        const h1WithTitleInfo = await $(".post").$("h2");
+        await expect(h1WithTitleInfo).toHaveText('Title test');
+
+        // check database persistence
+        const client = new Client({
+            user: 'postgres',
+            host: 'localhost',
+            database: 'test-postgres-demo',
+            password: 'mysecretpassword',
+            port: 5432,
+        });
+
+        await client.connect();
+        const res = await client.query('SELECT NOW()');
+        expect(res.rows.length).toEqual(1);
+    });
+});
+
+
