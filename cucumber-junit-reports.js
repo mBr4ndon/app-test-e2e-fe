8f3b26f19ec327d772d94c46c674c477006a4d43
Marcio Brandao (CTW) <Marcio.Brandao@ctw.bmwgroup.com>
2022-03-14 13:52:59 +0000

    Report in XML generation

--- cucumber-junit-reports.js
+++ cucumber-junit-reports.js
@@ -1,8 +1,8 @@
 const cucumberJunitConvert = require('cucumber-junit-convert');
 
 const options = {
-    inputJsonFile: 'cypress/cucumber-json/google.cucumber.json',
-    outputXmlFile: 'reports/xml/google.cucumber.xml',
+    inputJsonFile: 'cypress/cucumber-json/create-post.cucumber.json',
+    outputXmlFile: 'reports/xml/create-post.cucumber.xml',
     featureNameAsClassName: true // default: false
 }
 
